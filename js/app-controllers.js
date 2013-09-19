07f3cef4327eb5dbaf78cdeb4f230936ac38fe28
Miguel Roman <miguelerm@gmail.com>
2013-09-19 10:15:18 -0600

    Cambios sugeridos por JSLint (nada funcional)...

--- js/app-controllers.js
+++ js/app-controllers.js
@@ -1,7 +1,10 @@
-function TestController($scope, $timeout) {
+function TestController($scope, $timeout, $window) {
 
     'use strict';
-
+    
+    var amplify = $window.apmplify || { store : function () {} },
+        console = $window.console || { log : function () {} };
+    
     $scope.escribiendo = false;
     $scope.escritos = 0;
     $scope.capacidadDeDisco = amplify.store('capacidadDeDisco') || 500;
@@ -10,23 +13,23 @@ function TestController($scope, $timeout) {
     $scope.archivos = amplify.store('archivos') || [];
     $scope.archivoActual = null;
 
-    $scope.obtenerColorHex = function() {
-        return '#' + Math.floor(Math.random()*16777215).toString(16);
+    $scope.obtenerColorHex = function () {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16);
     };
 
     $scope.eliminar = function (archivo, $event) {
-        
-        for (var i = 0; i < $scope.archivos.length; i++) {
+        var i = 0, sector = null;
+        for (i = 0; i < $scope.archivos.length; i += 1) {
             if ($scope.archivos[i] === archivo) {
                 $scope.archivos.splice(i, 1);
             }
-        };
+        }
 
-        for (var i = 0; i < archivo.locacion.length; i++) {
-            var sector = archivo.locacion[i];
+        for (i = 0; i < archivo.locacion.length; i += 1) {
+            sector = archivo.locacion[i];
             $scope.sectores[sector].color = 255;
             $scope.sectores[sector].ocupado = false;
-        };
+        }
 
         amplify.store('archivos', $scope.archivos);
         $event.preventDefault();
@@ -55,40 +58,44 @@ function TestController($scope, $timeout) {
     };
 
     function inicializarSectores() {
-        for (var i = 0; i < $scope.capacidadDeDisco; i++) {
+        var i = 0, j = 0, sector = null, archivo = null;
+        for (i = 0; i < $scope.capacidadDeDisco; i += 1) {
 
-            var sector = { 
-                id: i, 
-                ocupado: false, 
-                actual: false, 
-                color: 255, 
-                siguiente: null,
+            sector = {
+                id: i,
+                ocupado: false,
+                actual: false,
+                color: 255,
+                siguiente: null
             };
 
             $scope.sectores.push(sector);
-        };
+        }
 
         if ($scope.archivos.length > 0) {
-            for (var i = 0; i < $scope.archivos.length; i++) {
-                var archivo = $scope.archivos[i];
-                for (var j = 0; j < archivo.locacion.length; j++) {
-                    var sector = archivo.locacion[j];
+            for (i = 0; i < $scope.archivos.length; i += 1) {
+                archivo = $scope.archivos[i];
+                for (j = 0; j < archivo.locacion.length; j += 1) {
+                    sector = archivo.locacion[j];
                     $scope.sectores[sector].ocupado = true;
                     $scope.sectores[sector].color = archivo.color;
-                };
-            };
+                }
+            }
         }
 
     }
 
     function marcarSectorActual() {
 
+        var actualIndex, actual, ultimoIndex, ultimo, anteriorIndex, anterior;
+        
         if ($scope.sectorActual === $scope.sectores.length - 1) {
             $scope.sectorActual = -1;
         }
 
-        var actualIndex = ++$scope.sectorActual;
-        var actual = $scope.sectores[actualIndex];
+        $scope.sectorActual += 1;
+        actualIndex = $scope.sectorActual;
+        actual = $scope.sectores[actualIndex];
         actual.actual = true;
 
         if ($scope.escribiendo && !actual.ocupado) {
@@ -97,10 +104,10 @@ function TestController($scope, $timeout) {
 
             actual.ocupado = true;
             actual.color = $scope.archivoActual.color;
-            $scope.escritos++;
+            $scope.escritos += 1;
             $scope.archivoActual.locacion.push(actualIndex);
 
-            if ($scope.escritos >= parseInt($scope.peso)) {
+            if ($scope.escritos >= parseInt($scope.peso, 10)) {
                 $scope.escritos = 0;
                 $scope.nombre = null;
                 $scope.peso = null;
@@ -113,14 +120,13 @@ function TestController($scope, $timeout) {
 
 
         if (actualIndex === 0) {
-            var ultimoIndex = $scope.sectores.length - 1;
-            var ultimo = $scope.sectores[ultimoIndex];
+            ultimoIndex = $scope.sectores.length - 1;
+            ultimo = $scope.sectores[ultimoIndex];
             ultimo.actual = false;
-        }
-        else {
-            var anteriorIndex = actualIndex - 1;
-            var anterior = $scope.sectores[anteriorIndex]; 
-            anterior.actual = false;    
+        } else {
+            anteriorIndex = actualIndex - 1;
+            anterior = $scope.sectores[anteriorIndex];
+            anterior.actual = false;
         }
 
         $timeout(marcarSectorActual, 1);
@@ -134,7 +140,3 @@ function TestController($scope, $timeout) {
     constructor();
 
 }
-
-$(function(){
-    //$('input[type=date]').datepicker();
-});
